3e33c65b48d2144f1f7e8c5b77ef372e43325956
Saqlain Ansari <ansarisaqlain987@gmail.com>
2025-05-07 13:27:10 +0530

    added docker config

--- src/index.ts
+++ src/index.ts
@@ -46,7 +46,7 @@ connectToDb()
     serve(
       {
         fetch: app.fetch,
-        port: 3001,
+        port: 3000,
       },
       (info) => {
         console.log(`Server is running on http://localhost:${info.port}`);
