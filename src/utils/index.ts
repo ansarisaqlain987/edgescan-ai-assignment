902b692a16178cabd382793c0c12e7d8d59fe090
Saqlain Ansari <ansarisaqlain987@gmail.com>
2025-05-14 15:33:24 +0530

    updated db port

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -19,10 +19,9 @@ export const getDatabaseString = () => {
   const DB_USER = process.env.DB_USER;
   const DB_PASS = process.env.DB_PASS;
   const DB_HOST = process.env.DB_HOST;
-  const DB_PORT = process.env.DB_PORT;
 
   if (isProduction) {
-    return `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+    return `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?authSource=admin`;
   }
 
   return `mongodb://${DB_USER}:${DB_PASS}@host.docker.internal:27017/${DB_NAME}?authSource=admin`;
