4a24d82d287e73ba4c1d4e36265354b878c067f1
Saqlain Ansari <ansarisaqlain987@gmail.com>
2025-05-14 16:03:25 +0530

    added mized property

--- src/models/content.ts
+++ src/models/content.ts
@@ -1,5 +1,6 @@
-import { getModelForClass, prop } from "@typegoose/typegoose";
+import { getModelForClass, modelOptions, prop } from "@typegoose/typegoose";
 
+@modelOptions({ options: { allowMixed: 0 } })
 class ContentClass {
   @prop({ type: String })
   public userId?: string;
